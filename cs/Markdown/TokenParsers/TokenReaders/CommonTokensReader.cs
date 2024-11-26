fa80658d860fc7012ae4e6e9f35c9a2586255e7a
S4MPAI <flekshum@gmail.com>
2024-11-26 13:18:08 +0500

    rewrite CommonTokensReader.cs

--- cs/Markdown/TokenParsers/TokenReaders/CommonTokensReader.cs
+++ cs/Markdown/TokenParsers/TokenReaders/CommonTokensReader.cs
@@ -1,3 +1,4 @@
+using Markdown.Extensions;
 using Markdown.Tokens;
 
 namespace Markdown.TokenParsers.TokenReaders;
@@ -6,27 +7,51 @@ public class CommonTokensReader : ITokenReader
 {
     public IEnumerable<Token> ReadTokens(string text)
     {
-        var left = 0;
-        
         for (var i = 0; i < text.Length; i++)
         {
-            var currentChar = text.Substring(i, 1);
-            
-            var nextToken = Token.TryCreateCommonToken(currentChar);
+            var currentChar = text.GetChar(i);
+            var tokenType = GetTokenType(currentChar);
 
-            if (nextToken == null) 
-                continue;
+            if (tokenType != TokenType.Tag && tokenType != TokenType.Word)
+                yield return Token.TryCreateCommonToken(currentChar)!.Value;
             
-            var wordLength = i - left;
-            if (wordLength > 0)
-                yield return Token.CreateWordToken(text.Substring(left, wordLength));
+            yield return CreateTokenAndMovePointer(text, tokenType, ref i);
+        }
+    }
 
-            left = i + 1;
-                
-            yield return nextToken.Value;
+    private static TokenType GetTokenType(string content)
+    {
+        var token = Token.TryCreateCommonToken(content);
+
+        if (token != null)
+            return token.Value.Type;
+        
+        var isTag = Token.IsTagStartPart(content);
+        
+        return isTag ? TokenType.Tag : TokenType.Word;
+    }
+    
+    private static Token CreateTokenAndMovePointer(string text, TokenType tokenType, ref int i)
+    {
+        var content = "";
+
+        while (i < text.Length && !IsTokenEnded(text.GetChar(i), content, tokenType))
+        {
+            content += text[i];
+            i++;
         }
 
-        if (left < text.Length)
-            yield return Token.CreateWordToken(text.Substring(left, text.Length - left));
+        i--;
+
+        return tokenType == TokenType.Tag
+            ? Token.TryCreateTagToken(content, out _)!.Value
+            : Token.CreateWordToken(content);
+    }
+
+    private static bool IsTokenEnded(string nextChar, string content, TokenType tokenType)
+    {
+        if (Token.TryCreateCommonToken(nextChar) != null || Token.IsTagStartPart(content))
+            return true;
+        return Token.TryCreateTagToken(content, out _) != null;
     }
 }
