90b24ce76a17e4ae8eadfb554484b7ce26ea5011
S4MPAI <flekshum@gmail.com>
2024-11-26 18:17:03 +0500

    add IncorrectTagTokensHandler

---
+++ cs/Markdown/TokenParsers/TokenHandlers/IncorrectTagTokensHandler.cs
@@ -0,0 +1,31 @@
+using Markdown.Tokens;
+
+namespace Markdown.TokenParsers.TokenHandlers;
+
+public class IncorrectTagTokensHandler : ITokenHandler
+{
+    public int Priority => 2;
+    public IReadOnlyList<Token> Handle(IReadOnlyList<Token> tokens)
+    {
+        var result = new List<Token>();
+        for (var i = 0; i < tokens.Count; i++)
+        {
+            var token = tokens[i];
+            
+            if (token.Type != TokenType.Tag)
+            {
+                result.Add(token);
+                continue;
+            }
+            
+            var tag = Token.GetTagByOpenTag(token);
+            
+            if (tag != null && (tag.IsCorrectOpenTag(tokens, i) || tag.IsCorrectCloseTag(tokens, i)))
+                result.Add(token);
+            else
+                result.Add(Token.CreateWordToken(token.Content));
+        }
+        
+        return result;
+    }
+}
