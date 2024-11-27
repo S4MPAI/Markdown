d7fbd1578c6e2dfe60fe1a48e3b6d7dfa7e2ae32
S4MPAI <flekshum@gmail.com>
2024-11-27 16:59:04 +0500

    fix int on var in loop of QueueExtensions.cs

--- cs/Markdown/Extensions/QueueExtensions.cs
+++ cs/Markdown/Extensions/QueueExtensions.cs
@@ -4,7 +4,7 @@ public static class QueueExtensions
 {
     public static IEnumerable<T> Dequeue<T>(this Queue<T> queue, int count)
     {
-        for (int i = 0; i < count; i++)
+        for (var i = 0; i < count; i++)
         {
             if (queue.Count == 0)
                 break;
