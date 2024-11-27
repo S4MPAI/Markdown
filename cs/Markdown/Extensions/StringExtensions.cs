6ac45bd7c036358d9f7640676c5667683cf03c4f
S4MPAI <flekshum@gmail.com>
2024-11-27 21:42:03 +0500

    rename StringExtensions.cs method

--- cs/Markdown/Extensions/StringExtensions.cs
+++ cs/Markdown/Extensions/StringExtensions.cs
@@ -2,6 +2,6 @@ namespace Markdown.Extensions;
 
 public static class StringExtensions
 {
-    public static string GetChar(this string str, int index) => 
+    public static string GetSymbol(this string str, int index) => 
         str.Substring(index, 1);
 }
